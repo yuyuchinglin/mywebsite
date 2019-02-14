4881683bcf27539a651d89cb6c0e57b2575083c2
yuyuchinglin <yuyuchinglin@gmail.com>
2019-02-15 00:17:33 +0800

    NEW VERSION!!!!!!!

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,58 +1,4 @@
-var navItem = document.querySelectorAll('.navbar-nav .nav-item');
-    var subNavbar = document.querySelectorAll('.sub-navbar');
-    var port = document.querySelector('#port');
 
-    navItem.forEach(function(item){
-
-        item.addEventListener("mouseover", function(item) {
-
-        subNavbar.forEach(function(sub){
-          if(!sub.classList.contains('d-none')){
-              sub.classList.add('d-none')
-          }
-        })
-
-        document.querySelector('.'+item.target.name+'-subnavbar').classList.remove('d-none');
-        })
-
-    })
-
-
-    port.addEventListener("mouseover", function(item) {
-        subNavbar.forEach(function(sub){
-          if(!sub.classList.contains('d-none')){
-              sub.classList.add('d-none')
-          }
-        })
-    })
-
-
-    // document.querySelectorAll('.left-navbar a').forEach(function(item){
-    //     console.log(item);
-    //     item.addEventListener("mouseover", function(e){
-    //         var thisimg = e.target.querySelector('img');
-    //         // document.querySelectorAll('.left-navbar img').forEach(function(img){
-    //         //     if(img!=thisimg){
-    //         //         if(!img.classList.contains('d-none')){
-    //         //             img.classList.add('d-none');
-    //         //         }
-    //         //     }
-                
-    //         // })
-    //         if(e.target.querySelector('img').classList.contains('d-none')){
-    //             e.target.querySelector('img').classList.remove('d-none')
-    //         }else{
-    //             e.target.querySelector('img').classList.add('d-none')
-    //         };
-    //     }),
-    //     item.addEventListener("mouseout", function(e){
-    //         //console.log(e.target.querySelector('img'));
-    //         // e.target.querySelector('img').classList.add('d-none');
-    //     })
-    // })
-
-
-    $(function() {
         var Accordion = function(el, multiple) {
             this.el = el || {};
             this.multiple = multiple || false;
@@ -79,16 +25,83 @@ var navItem = document.querySelectorAll('.navbar-nav .nav-item');
         }	
     
         var accordion = new Accordion($('#accordion'), false);
-    });
 
 
-    // function dropdown(this){
-    //     console.log(this);
-    //     this.dropdown;
-    // }
 
-    // var a = document.querySelector('.accordion .submenu li a');
+        // slick (MORE PORTFOLIO)
 
-    // a.on('mouseover', function(){
-    //     // a.que
-    // })
+        if (document.body.clientWidth > 900) {
+			$('.more').slick({
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				autoplay: true,
+				autoplaySpeed: 4000,
+				dots: true,
+			});
+		} else {
+			$('.more').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+				autoplaySpeed: 2000,
+				dots: false,
+			});
+        }
+        
+
+        //	NAVBAR	
+        
+        function openNav() {
+			document.getElementById("mySidenav").style.right = "-17px";
+			document.querySelector('.window-overlay').style.display = "block";
+		}
+
+
+		function closeNav() {
+			document.getElementById("mySidenav").style.right = "-417px";
+			document.querySelector('.window-overlay').style.display = "none";
+        }
+        
+
+
+        // Hide Header on on scroll down
+		var didScroll;
+		var lastScrollTop = 0;
+		var delta = 5;
+		var navbarHeight = $('header').outerHeight();
+
+		$(window).scroll(function (event) {
+			didScroll = true;
+		});
+
+		setInterval(function () {
+			if (didScroll) {
+				hasScrolled();
+				didScroll = false;
+			}
+		}, 250);
+
+		function hasScrolled() {
+			var st = $(this).scrollTop();
+
+			// Make sure they scroll more than delta
+			if (Math.abs(lastScrollTop - st) <= delta)
+				return;
+
+			// If they scrolled down and are past the navbar, add class .nav-up.
+			// This is necessary so you never see what is "behind" the navbar.
+			if (st > lastScrollTop && st > navbarHeight) {
+				// Scroll Down
+				$('header').removeClass('nav-down').addClass('nav-up');
+			} else {
+				// Scroll Up
+				if (st + $(window).height() < $(document).height()) {
+					$('header').removeClass('nav-up').addClass('nav-down');
+				}
+			}
+
+			lastScrollTop = st;
+		}
+
+
+

44da0f46299926d66c75a8231a6118848a27ddd4
yuching_uq <yuching_lin@ubiqconn.com>
2019-12-27 17:21:12 +0800

    slick

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,9 +1,30 @@
-$('.port').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 2000,
-});
+
+if ($(window).width() < 750) {
+  $('.port').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+  });
+} else if ($(window).width() < 970) {
+  $('.port').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: false,
+  });
+}
+else {
+  $('.port').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+  });
+};
+
+
+
+
+
+
 
 
 
